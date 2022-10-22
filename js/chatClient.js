f2a05bf940518b51df5efd4d204ae46e3bf499f4
Emeric <emeric.guichet@gmail.com>
2022-10-22 12:09:18 +0200

    ajout des evenements de bouton

--- js/chatClient.js
+++ js/chatClient.js
@@ -1,35 +1,87 @@
-var login = prompt('Entrez votre pseudo :');
 var websocket;
 
-// Initialise le Web Socket
+// Initialize the webSocket
 createWebSocket();
 
 function createWebSocket() {
-    websocket= new WebSocket("ws://localhost:12345/");
+    websocket= new WebSocket("ws://localhost:9000/");
 }
 
-// Envoie au serveur le message
-function sendMessage() {
-    let content = document.getElementById("msg").value;//recupere le message de l'input
-    document.getElementById("msg").value = "";//on vide l'élément
-    websocket.send("["+login+"] "+content);//on envoie le msg au serveur + login de la personne
+function addWordToList(word){
+    let temporaryTableList = document.getElementById("temporary_list");
+        let newNumber = temporaryTableList.childElementCount + 1;
+        let element = "<tr><td>"+newNumber+"</td><td>"+word+"</td></tr>";
+        temporaryTableList.insertAdjacentHTML("beforeend", element);
 }
+
+
+//add a word to the temporary list
+function addWord(){
+    let wordToAdd = document.getElementById("wordToAdd").value;
+    if(wordToAdd !== ""){
+        addWordToList(wordToAdd);
+    }
+}
+
+
+// read file and check the extension
+function readFile(){
+    let temporaryList = document.getElementById("temporary_list");
+    temporaryList.innerHTML = "";
+    var file = new FileReader();
+    let nameFile = this.files[0].name;
+    let extension = /(\.txt)$/i;
+    if (!extension.exec(nameFile))
+	{
+		console.log('Format de fichier non valide');
+	}else{
+        file.onload = () => {
+            let listSplit = file.result.split(/\r\n|\n/);
+            for(let i=0; i<listSplit.length -1;i++){
+                addWordToList(listSplit[i]);
+            }
+        }
+    }
+
+    file.readAsText(this.files[0]);
+}
+
+document.getElementById('upload_file').addEventListener('change', readFile);
+
+
+
+function generate(){
+    let temporaryList = document.getElementById("temporary_list");
+
+    if(temporaryList.childElementCount > 0){
+        let childrens = temporaryList.children;
+        let concatToSend = "";
+        for(let i=0; i< childrens.length; i++){
+            let element = childrens[i].lastElementChild.textContent;
+            concatToSend += element+";";
+        }
+        websocket.send(concatToSend);
+    }
+}
+
 
 // Réception du message envoyé par le serveur + Affichage
 websocket.onmessage = function (event) {
-    let tchat = document.getElementById("messages");//on recupere le textarea
-    tchat.setAttribute( "disabled", false);
-    tchat.append(event.data+"\n");
-    tchat.setAttribute("disabled", true);
+    let generateList = document.getElementById("generate_list");
+    generateList.innerHTML = "";
+    let receiveData = event.data.split(";");
+    for(let i=0; i<receiveData.length;i++){
+        let addHtml = "<tr><td>"+(i+1)+"</td><td>"+receiveData[i]+"</td></tr>";
+        generateList.insertAdjacentHTML("beforeend", addHtml);
+    }
 };
 
 // Notification de connexion
 websocket.onopen = function () {
-    websocket.send(login+" a rejoint le canal de discussion");
+    console.log("Connection to the server successfully established");
 }
 
 // Notification de déconnexion (jamais appelée)
 websocket.onclose = function () {
-    console.log('FERMETURE');
     websocket.send(login+" a quitté le canal de discussion");
 }
